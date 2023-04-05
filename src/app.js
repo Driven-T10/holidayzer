fdc15c3d86ecfa8e54f55f16eed72808911cf78a
leticia-chijo <leticia.chijo@gmail.com>
2023-04-05 14:28:19 -0300

    Estado inicial do exercício

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const holidays = [
+	{ date: "1/1/2023", name: "Confraternização mundial" },
+	{ date: "2/2/2023", name: "Carnaval" },
+	{ date: "4/7/2023", name: "Sexta-feira Santa" },
+	{ date: "4/9/2023", name: "Páscoa" },
+	{ date: "4/21/2023", name: "Tiradentes" },
+	{ date: "5/1/2023", name: "Dia do trabalho" },
+	{ date: "6/8/2023", name: "Corpus Christi" },
+	{ date: "9/7/2023", name: "Independência do Brasil" },
+	{ date: "10/12/2023", name: "Nossa Senhora Aparecida" },
+	{ date: "11/2/2023", name: "Finados" },
+	{ date: "11/15/2023", name: "Proclamação da República" },
+	{ date: "12/25/2023", name: "Natal" }
+]
